13f580ce873735b69270b73e158579cdc92beb2b
jpduloch <roland.mosimann@students.fhnw.ch>
2017-05-21 13:55:13 +0200

    added md5 function

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
-class Main{
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.*;
+import java.util.Arrays;
+
+class Main {
 
   public static void main(String[] args) {
-    System.out.println("hello");
+
+
+
   }
 
 
+
+
+  public static String callculateMd5(String input) {
+    try {
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      byte[] messageDigest = md.digest(input.getBytes());
+      BigInteger number = new BigInteger(1, messageDigest);
+      String hashtext = number.toString(16);
+      while (hashtext.length() < 32) {
+        hashtext = "0" + hashtext;
+      }
+      return hashtext;
     }
+    catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
