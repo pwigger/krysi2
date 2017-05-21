e583bb883716fb2c174448b2a0afcb3f66215289
jpduloch <roland.mosimann@students.fhnw.ch>
2017-05-21 15:50:08 +0200

    created reduction function

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,78 @@ class Main {
 
   public static void main(String[] args) {
 
-    for (int i = 0; i < 2000; i++) {
-      System.out.println(pwGenerator(i));
-
-    }
-
+    reductionFunction("29c3eea3f305d6b823f562ac4be35217", 0);
   }
 
-  public static String pwGenerator(int i) {
-    String pw = "0000000";
 
-    for (int j = 0; j < i; j++) {
-      pw  = raiseChar(pw, 6);
+
+  /**
+   * Reduces given String to 7 Chars. Follows ReductionFunction on Slide 3.27.
+   *
+   * @param hash Hash to reduce.
+   * @param stufe Step, in that the fuction is called, influences the result.
+   * @return the reduced String.
+   */
+  public static String reductionFunction(String hash, int stufe) {
+    StringBuilder output = new StringBuilder("0000000");
+
+    //Create array of all Chars
+    char[] signs = new char[36];
+    for (int i = 0; i < 10; i++) {
+      signs[i] = (char) (i + 48);
+    }
+    for (int i = 10; i < 36; i++) {
+      signs[i] = (char) (i + 87);
     }
 
+    BigInteger step = BigInteger.valueOf(stufe);
+    BigInteger arrLength = BigInteger.valueOf(signs.length);
+
+    //TODO: Gegebener HAshwert als natürliche Zahl.
+    BigInteger hashNumber = new BigInteger(hash);
+
+
+    hashNumber.add(step);
+
+    for (int i = 6; i >= 0; i--) {
+
+      char toset = (char) hashNumber.mod(arrLength).intValue();
+      output.setCharAt(i, toset);
+
+      hashNumber = hashNumber.divide(arrLength);
+    }
+
+    return output.toString();
+  }
+
+
+
+  /**
+   * Generates the i'st password in the list.
+   *
+   * @param i n- of what password to generate.
+   * @return the password.
+   */
+  public static String pwGenerator(int i) {
+    String pw = "0000000";
+    for (int j = 0; j < i; j++) {
+      pw = raiseChar(pw, 6);
+    }
     return pw;
   }
 
 
-  public static String raiseChar(String text, int pos) {
+
+  /**
+   * Raises the passwords chars according to dfinition [0-9][a-z] recursive.
+   *
+   * @param text inputs the pw to raise.
+   * @param pos  char position that has to be risen.
+   * @return raised pw.
+   */
+  private static String raiseChar(String text, int pos) {
     StringBuilder pw = new StringBuilder(text);
     int actual = pw.charAt(pos);
-
-//    System.out.println(text +"  "+ pos +"  "+actual);
 
     if (actual >= 48 && actual <= 56) {
       pw.setCharAt(pos, (char) (actual + 1));
@@ -37,16 +86,23 @@ class Main {
     } else if (actual >= 97 && actual <= 121) {
       pw.setCharAt(pos, (char) (actual + 1));
     } else if (actual == 122) {
-//      System.out.println("NOW: " +pw.toString());
       String received = raiseChar(pw.toString(), (pos - 1));
-      pw.delete(0,7).append(received);
+      pw.delete(0, 7).append(received);
       pw.setCharAt(pos, '0');
     }
-
     return pw.toString();
-
   }
 
+
+
+
+  /**
+   * Callculates a MD5 hash to a string.
+   * copied from: http://www.asjava.com/core-java/java-md5-example/
+   *
+   * @param input String to create hash from.
+   * @return MD5 hash from String.
+   */
   public static String callculateMd5(String input) {
     try {
       MessageDigest md = MessageDigest.getInstance("MD5");
