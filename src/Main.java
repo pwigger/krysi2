b54ff6ac19ed3cd0058dee1286efa8180b5f752d
jpduloch <roland.mosimann@students.fhnw.ch>
2017-05-21 14:52:02 +0200

    added pw-generator

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.*;
 import java.util.Arrays;
@@ -7,12 +6,46 @@ class Main {
 
   public static void main(String[] args) {
 
+    for (int i = 0; i < 2000; i++) {
+      System.out.println(pwGenerator(i));
 
+    }
 
   }
 
+  public static String pwGenerator(int i) {
+    String pw = "0000000";
+
+    for (int j = 0; j < i; j++) {
+      pw  = raiseChar(pw, 6);
+    }
+
+    return pw;
+  }
 
 
+  public static String raiseChar(String text, int pos) {
+    StringBuilder pw = new StringBuilder(text);
+    int actual = pw.charAt(pos);
+
+//    System.out.println(text +"  "+ pos +"  "+actual);
+
+    if (actual >= 48 && actual <= 56) {
+      pw.setCharAt(pos, (char) (actual + 1));
+    } else if (actual == 57) {
+      pw.setCharAt(pos, 'a');
+    } else if (actual >= 97 && actual <= 121) {
+      pw.setCharAt(pos, (char) (actual + 1));
+    } else if (actual == 122) {
+//      System.out.println("NOW: " +pw.toString());
+      String received = raiseChar(pw.toString(), (pos - 1));
+      pw.delete(0,7).append(received);
+      pw.setCharAt(pos, '0');
+    }
+
+    return pw.toString();
+
+  }
 
   public static String callculateMd5(String input) {
     try {
@@ -24,8 +57,7 @@ class Main {
         hashtext = "0" + hashtext;
       }
       return hashtext;
-    }
-    catch (NoSuchAlgorithmException e) {
+    } catch (NoSuchAlgorithmException e) {
       throw new RuntimeException(e);
     }
   }
